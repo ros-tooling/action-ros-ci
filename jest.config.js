ff7cf4de3230e49a0695e8d43e27aa40c9765087
Anas Abou Allaban <allabana@amazon.com>
2020-01-22 15:53:24 -0800

    Add comment for jest
    Signed-off-by: Anas Abou Allaban <allabana@amazon.com>

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   clearMocks: true,
+  // Removing 'js' throws a Validation Error: moduleFileExtensions must include 'js'
   moduleFileExtensions: ['ts', 'js'],
   testEnvironment: 'node',
   testMatch: ['**/__tests__/*.test.ts','**/*.test.ts'],
