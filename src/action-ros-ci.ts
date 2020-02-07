6f04d32ace056e5f222b2737f31a74d8ce3340a7
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-02-07 22:04:38 +0100

    Support for coverage-ignore-pattern input param
    Implement an input parameter called coverage-ignore-pattern in order to
    provide colcon lcov-result --filter option a pattern for exclusion in
    coverage generated information.

--- src/action-ros-ci.ts
+++ src/action-ros-ci.ts
@@ -95,6 +95,7 @@ async function run() {
 		const vcsRepoFileUrl = resolveVcsRepoFileUrl(
 			core.getInput("vcs-repo-file-url", { required: true })
 		);
+		const coverageIgnorePattern = core.getInput("coverage-ignore-pattern");
 
 		let commandPrefix = "";
 		if (sourceRosBinaryInstallation) {
@@ -230,9 +231,9 @@ async function run() {
 
 		// ignoreReturnCode is set to true to avoid having a lack of coverage
 		// data fail the build.
-		const colconLcovResultCmd = `colcon lcov-result --packages-select ${packageNameList.join(
-			" "
-		)}`;
+		const colconLcovResultCmd = `colcon lcov-result \
+	             --filter ${coverageIgnorePattern} \
+	             --packages-select ${packageNameList.join(" ")}`;
 		await execBashCommand(colconLcovResultCmd, commandPrefix, {
 			cwd: rosWorkspaceDir,
 			ignoreReturnCode: true
