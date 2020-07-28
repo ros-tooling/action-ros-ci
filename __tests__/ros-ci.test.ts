41b0a69eef289291f74727adfd95b645bcfa119d
Jesse Ikawa <jikawa@amazon.com>
2020-07-28 01:46:44 -0700

    add unit test
    Signed-off-by: Jesse Ikawa <jikawa@amazon.com>

--- __tests__/ros-ci.test.ts
+++ __tests__/ros-ci.test.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as actionRosCi from "../src/action-ros-ci"
 import {execBashCommand} from '../src/action-ros-ci'
 
 jest.setTimeout(20000);  // in milliseconds
@@ -26,3 +27,32 @@ describe('execBashCommand test suite', () => {
     expect(result).not.toEqual(0);
   })
 })
+
+describe("validate distribution test", () => {
+	it("test valid", async () => {
+		await expect(actionRosCi.validateDistro("kinetic", "", "")).toBe(true);
+		await expect(actionRosCi.validateDistro("lunar", "", "")).toBe(true);
+		await expect(actionRosCi.validateDistro("melodic", "", "")).toBe(true);
+		await expect(actionRosCi.validateDistro("noetic", "", "")).toBe(true);
+		await expect(actionRosCi.validateDistro("", "dashing", "")).toBe(true);
+		await expect(actionRosCi.validateDistro("", "eloquent", "")).toBe(true);
+		await expect(actionRosCi.validateDistro("", "foxy", "")).toBe(true);
+	});
+
+	it("test not valid", async () => {
+		//ROS1 End-of-Life
+		await expect(actionRosCi.validateDistro("box", "", "")).toBe(false);
+		await expect(actionRosCi.validateDistro("c", "", "")).toBe(false);
+		await expect(actionRosCi.validateDistro("diamondback", "", "")).toBe(false);
+		await expect(actionRosCi.validateDistro("electric", "", "")).toBe(false);
+		await expect(actionRosCi.validateDistro("fuerte", "", "")).toBe(false);
+		await expect(actionRosCi.validateDistro("groovy", "", "")).toBe(false);
+		await expect(actionRosCi.validateDistro("hydro", "", "")).toBe(false);
+		await expect(actionRosCi.validateDistro("indigo", "", "")).toBe(false);
+		await expect(actionRosCi.validateDistro("jade", "", "")).toBe(false);
+		//ROS2 End-of-Life
+		await expect(actionRosCi.validateDistro("", "ardent", "")).toBe(false);
+		await expect(actionRosCi.validateDistro("", "bouncy", "")).toBe(false);
+		await expect(actionRosCi.validateDistro("", "crystal", "")).toBe(false);
+	});
+});
