03ccd255c9eea52721df60b36121f71be8fef75f
Dan Rose <dan@digilabs.io>
2020-09-08 16:48:03 -0500

    Make sure rebuilt index.js gets added to git
    I had assumed that lint-staged would auto-add any file changed by the pre-commit step.
    This is wrong - if dist/index.js has no changes before the commit, we need to `git add` it.
    
    Signed-off-by: Dan Rose <dan@digilabs.io>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,10 @@ module.exports = {
 	"*": [
 		"eslint --cache --fix --ignore-path .lintignore",
 		"prettier --write --ignore-path .lintignore --ignore-unknown",
-		(files) => "npm run build",
-		(files) => "npm run test",
+		// Note: doing the build here ensures we omit unstaged changes
+		() => "npm run build",
+		() => "git add dist/index.js",
+		// same here. Only test staged changes.
+		() => "npm run test",
 	],
 };
