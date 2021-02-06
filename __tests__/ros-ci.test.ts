136b719dc05c43563e7dad8a4f4e9993b1bb6887
Christophe Bedard <bedard.christophe@gmail.com>
2021-02-06 15:41:52 -0500

    Support overriding repos files & providing supplemental ones in PRs
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- __tests__/ros-ci.test.ts
+++ __tests__/ros-ci.test.ts
@@ -1,5 +1,6 @@
 import * as core from "@actions/core";
 import * as actionRosCi from "../src/action-ros-ci";
+import * as dep from "../src/dependencies";
 import { execBashCommand } from "../src/action-ros-ci";
 
 jest.setTimeout(20000); // in milliseconds
@@ -37,5 +38,57 @@ describe("validate distribution test", () => {
 		expect(actionRosCi.validateDistros("groovy", "")).toBe(false);
 		expect(actionRosCi.validateDistros("", "bouncy")).toBe(false);
 		expect(actionRosCi.validateDistros("apples", "bananas")).toBe(false);
+	});
+});
+
+describe("PR-specific repos files", () => {
+	it("should not do anything if not a PR", async () => {
+		let payload = {};
+		expect(dep.getReposFilesOverride(payload)).toEqual([]);
+		expect(dep.getReposFilesSupplemental(payload)).toEqual([]);
+		payload = { pull_request: {} };
+		expect(dep.getReposFilesOverride(payload)).toEqual([]);
+		expect(dep.getReposFilesSupplemental(payload)).toEqual([]);
+		payload = { pull_request: { body: "" } };
+		expect(dep.getReposFilesOverride(payload)).toEqual([]);
+		expect(dep.getReposFilesSupplemental(payload)).toEqual([]);
+	});
+
+	it("should extract repos files from the PR body", () => {
+		const bodyEmpty = `
+Description of the changes.
+Blah blah.
+`;
+		let payload = {};
+		payload = { pull_request: { body: bodyEmpty } };
+		expect(dep.getReposFilesOverride(payload)).toEqual([]);
+		expect(dep.getReposFilesSupplemental(payload)).toEqual([]);
+
+		const body = `
+Description of the changes.
+Blah blah.
+
+action-ros-ci-repos-override:   
+action-ros-ci-repos-override: https://raw.githubusercontent.com/ros2/ros2/master/ros2.repos
+action-ros-ci-repos-override : https://some.website.repos
+ action-ros-ci-repos-override:  https://gist.github.com/some-user/some-gist.repos
+ action-ros-ci-repos-supplemental:https://gist.github.com/some-user/some-other-gist.repos
+action-ros-ci-repos-supplemental:  file://path/to/some/file.txt 
+`;
+		payload = { pull_request: { body: body } };
+		const expectedOverride = [
+			"https://raw.githubusercontent.com/ros2/ros2/master/ros2.repos",
+			"https://gist.github.com/some-user/some-gist.repos",
+		];
+		const expectedSupplemental = [
+			"https://gist.github.com/some-user/some-other-gist.repos",
+			"file://path/to/some/file.txt",
+		];
+		expect(dep.getReposFilesOverride(payload)).toEqual(
+			expect.arrayContaining(expectedOverride)
+		);
+		expect(dep.getReposFilesSupplemental(payload)).toEqual(
+			expect.arrayContaining(expectedSupplemental)
+		);
 	});
 });
