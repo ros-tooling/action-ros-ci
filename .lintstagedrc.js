d564c4122eb005ced0db365ebc43c3275e006ad0
Christophe Bedard <bedard.christophe@gmail.com>
2021-03-06 13:24:27 -0500

    Explicitly provide .eslintignore to eslint & prettier
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"*": [
-		"eslint --cache --fix",
-		"prettier --write --ignore-unknown",
+		"eslint --cache --fix --ignore-path .eslintignore",
+		"prettier --write --ignore-path .eslintignore --ignore-unknown",
 		// Note: doing the build here ensures we omit unstaged changes
 		() => "npm run build",
 		() => "git add dist/index.js",
