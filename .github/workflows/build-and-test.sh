e48934fcd3789be3d6e30c4150ea6d2621d98428
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2026-05-14 17:34:36 +0200

    Add setuptools installation to build-and-test script
    Added pip install for setuptools to the build script.

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -3,6 +3,8 @@ set -euxo pipefail
 
 npm install -g codecov
 
+pip install setuptools
+
 # When running inside a Docker container, by default, we're root and all files belond to root.
 # However, calling the npm scripts (build, etc.) with 'npm run ...' runs the commands as user
 # ID=1001, which means we can't open or write to any files. Therefore, if we're in Docker, chown
